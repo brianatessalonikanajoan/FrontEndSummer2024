a9385daf46d3796b3c1f8765d22db937d35833b9
brianatessalonikanajoan <s22210456@student.unklab.ac.id>
2024-06-20 17:00:12 -0800

    adding console.log4

--- pertemuan-14/index.js
+++ pertemuan-14/index.js
@@ -1,3 +1,4 @@
 //working ion remote repository
-console.log("Hello Git and GitHub")
-console.log("testing merge confict deal vs code")
+console.log("Hello git and GitHub")
+console.log("testing merge confict dari vs code")
+console.log("testing merge confict")
